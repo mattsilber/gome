c9240ab610721deea2fb8535b26f054f8105f721
Matthew Silber <cruceo@guardanis.com>
2016-09-18 13:22:47 -0400

    Added a launcher icon cuz I felt like playing with inkscape

--- gome-android-client/src/main/java/com/guardanis/gome/SplashActivity.java
+++ gome-android-client/src/main/java/com/guardanis/gome/SplashActivity.java
@@ -5,18 +5,35 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.guardanis.gome.tools.views.ToolbarLayoutBuilder;
+import com.guardanis.imageloader.ImageRequest;
 
 public class SplashActivity extends BaseActivity {
+
+    private Handler launchHandler = new Handler();
 
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(() -> {
+        ImageRequest.create(findViewById(R.id.splash__image))
+                .setTargetAsset("gome__ic_launcher.svg")
+                .setFadeTransition()
+                .setShowStubOnExecute(false)
+                .setShowStubOnError(false)
+                .execute(150);
+
+        launchHandler.postDelayed(() -> {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }, 2000);
+    }
+
+    @Override
+    protected void onDestroy(){
+        launchHandler.removeCallbacksAndMessages(null);
+
+        super.onDestroy();
     }
 
     @Override
