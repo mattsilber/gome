c84a3bd5e29ec2f8aad79a1292004ee4885c9ed5
Matt Silber <cruceo@guardanis.com>
2016-11-02 21:21:47 -0400

    Added pref for ip

--- gome-swift-client/gome/ConnectViewController.swift
+++ gome-swift-client/gome/ConnectViewController.swift
@@ -2,7 +2,11 @@ import UIKit
 
 class ConnectViewController: UIViewController, Navigable {
     
+    private let KEY__LAST_IP = "last_ip_address"
+    
     @IBOutlet weak var ipInputView: UITextView!
+    
+    var preferences = UserDefaults.standard
     
     func onControllerOpened(_ from: Nav.Route, params: [String: Any]?) {
         
@@ -20,6 +24,10 @@ class ConnectViewController: UIViewController, Navigable {
         super.viewDidLoad()
         
         Nav.instance.takeRootOwnership(self)
+        
+        if let lastIp = preferences.object(forKey: KEY__LAST_IP) as? String {
+            ipInputView.text = lastIp
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +36,9 @@ class ConnectViewController: UIViewController, Navigable {
     }
     
     @IBAction func onConnectClicked(_ sender: Any){
+        preferences.set(ipInputView.text, forKey: KEY__LAST_IP)
+        preferences.synchronize()
+        
         var params = [String: Any]()
         params[ControllerViewController.PARAM__IP_ADDRESS] = ipInputView.text
         
