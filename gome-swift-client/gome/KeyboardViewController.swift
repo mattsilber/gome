124ee5173f11c0a96ae65e07854e555a970217ea
Matt Silber <cruceo@guardanis.com>
2016-11-09 21:32:27 -0500

    Added active states for wrapped actions

--- gome-swift-client/gome/KeyboardViewController.swift
+++ gome-swift-client/gome/KeyboardViewController.swift
@@ -6,6 +6,10 @@ class KeyboardViewController: UIViewController, Navigable {
     private let ACTION__CONTROL = "CTRL"
     private let ACTION__SHIFT = "SHIFT"
     
+    @IBOutlet weak var keyAlt: UIButton!
+    @IBOutlet weak var keyControl: UIButton!
+    @IBOutlet weak var keyShift: UIButton!
+    
     private var wrappedValues = [String]()
 
     func onControllerOpened(_ from: Nav.Route, params: [String: Any]?) {
@@ -35,25 +39,31 @@ class KeyboardViewController: UIViewController, Navigable {
     private func sendKey(_ key: String){
         SocketManager.instance.write(KeyboardCommand(key, wrappedValues: wrappedValues))
         wrappedValues = [String]()
+        
+        keyAlt.backgroundColor = nil
+        keyControl.backgroundColor = nil
+        keyShift.backgroundColor = nil
     }
     
     @IBAction func onKeyAltClicked(_ sender: Any){
-        toggle(ACTION__ALT)
+        toggle(ACTION__ALT, keyAlt)
     }
     
     @IBAction func onKeyControlClicked(_ sender: Any){
-        toggle(ACTION__CONTROL)
+        toggle(ACTION__CONTROL, keyControl)
     }
     
     @IBAction func onKeyShiftClicked(_ sender: Any){
-        toggle(ACTION__SHIFT)
+        toggle(ACTION__SHIFT, keyShift)
     }
     
-    private func toggle(_ key: String){
+    private func toggle(_ key: String, _ button: UIButton){
         if wrappedValues.contains(key) {
             wrappedValues.remove(at: wrappedValues.index(of: key)!)
+            button.backgroundColor = nil
         } else {
             wrappedValues.append(key)
+            button.backgroundColor = UIColor.darkGray
         }
     }
     
