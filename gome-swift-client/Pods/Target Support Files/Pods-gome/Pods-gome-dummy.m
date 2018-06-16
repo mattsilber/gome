bc64c9214289b58a80824df28a388a694e666bb4
Matt Silber <cruceo@guardanis.com>
2018-06-15 21:42:40 -0400

    Pods shouldn't have been in the repo.....

--- gome-swift-client/Pods/Target Support Files/Pods-gome/Pods-gome-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_gome : NSObject
-@end
-@implementation PodsDummy_Pods_gome
-@end
--- gome-swift-client/Pods/Target Support Files/Pods-gomeTests/Pods-gomeTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_gomeTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_gomeTestsVersionString[];
-
