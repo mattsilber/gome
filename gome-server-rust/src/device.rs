f43f8a7539ea5736d0ffa160da034d49a7165069
Matt Silber <cruceo@guardanis.com>
2020-02-02 10:48:50 -0500

    I think I like collect better

--- gome-server-rust/src/device.rs
+++ gome-server-rust/src/device.rs
@@ -2,18 +2,18 @@ extern crate pnet;
 use pnet::datalink;
 use std::iter;
 
-pub fn with_local_ip_addresses(each: fn(String)) {
-    datalink::interfaces()
+pub fn collect_local_ip_addresses() -> Vec<String> {
+    return datalink::interfaces()
         .iter()
         .flat_map(|it| it.ips.iter())
         .filter(|it| it.is_ipv4())
         .map(|it| it.ip().to_string())
         .filter(|it| !it.eq_ignore_ascii_case("127.0.0.1"))
-        .for_each(each);
+        .collect();
 }
 
 pub fn print_local_ip_addresses() {
-    return with_local_ip_addresses(|it|
-        println!("Local IP: {:?}", it)
-    )
+    collect_local_ip_addresses()
+        .iter()
+        .for_each(|it| println!("Local IP: {:?}", it))
 }
